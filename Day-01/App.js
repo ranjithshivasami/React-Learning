80eb7a20b370eaaaf4e2fb950ea99bc20be1985f
Ranjith Sivasami <ranjithshivasami@gmail.com>
2022-12-24 23:25:18 +0530

    day 1 assigenment

---
+++ Day-01/App.js
@@ -0,0 +1,14 @@
+const elementH1 = React.createElement('h1', {
+    'id': 'heading-1'
+}, 'Namaste React');
+const heading1 = React.createElement('h1', 
+{ id: 'heading-1' },
+ 'Heading 1');
+const heading2 = React.createElement('h2',
+ { id: 'heading-2' },
+  'Heading 2');
+const container = React.createElement('div',
+ { id: 'container' },
+  [heading1, heading2]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
